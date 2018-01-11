0b035ba76911bb35c1d2d294025182569e72ee2b
Gwen Latasa <gxlatasa@gmail.com>
2018-01-10 20:10:54 -0500

    more map test

--- public/js/app.js
+++ public/js/app.js
@@ -35,17 +35,17 @@ $('.timepicker').pickatime({
   );
 
 //https://developers.google.com/maps/documentation/javascript/adding-a-google-map#key
-  // function initMap() {
-  //         var uluru = {lat: -25.363, lng: 131.044};
-  //         var map = new google.maps.Map(document.getElementById('map'), {
-  //           zoom: 4,
-  //           center: uluru
-  //         });
-  //         var marker = new google.maps.Marker({
-  //           position: uluru,
-  //           map: map
-  //         });
-  //       }
+  function initMap() {
+          var uluru = {lat: -25.363, lng: 131.044};
+          var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+          });
+          var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+          });
+        }
 
 //w3Schools
   function validateSignup() {
